19e73e9e29698103b242215dd8fb27dab9408021
Andrey <kaban4ik19901990@mail.ru>
2015-04-02 14:16:02 +0300

    Added move logic for rook.

--- Chess.Core/Mediator/RookColleague.cs
+++ Chess.Core/Mediator/RookColleague.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Chess.Core.Enums;
 using Chess.Core.Models;
 
 namespace Chess.Core.Mediator
@@ -7,12 +8,161 @@ namespace Chess.Core.Mediator
     {
         public override IEnumerable<Position> GetPossibleMoves(Position figurePosition, IChessboard chessboard)
         {
-            throw new System.NotImplementedException();
+            var result = new List<Position>();
+            var isEndLine = false;
+            var testPosition = new Position(figurePosition);
+
+            while (!isEndLine) //y up
+            {
+                testPosition.Y = chessboard.IncrementY(testPosition.Y);
+                if (testPosition.Y != -1 && chessboard.GetFigureByPosition(testPosition) == null)
+                {
+                    result.Add(new Position(testPosition));
+                }
+                else isEndLine = true;
+            }
+
+            testPosition = new Position(figurePosition);
+            isEndLine = false;
+            while (!isEndLine) //y down
+            {
+                testPosition.Y = chessboard.DecrementY(testPosition.Y);
+                if (testPosition.Y != -1 && chessboard.GetFigureByPosition(testPosition) == null)
+                {
+                    result.Add(new Position(testPosition));
+                }
+                else isEndLine = true;
+            }
+
+            testPosition = new Position(figurePosition);
+            isEndLine = false;
+            while (!isEndLine) //x up
+            {
+                testPosition.X = chessboard.IncrementX(testPosition.X);
+                if (testPosition.X != ' ' && chessboard.GetFigureByPosition(testPosition) == null)
+                {
+                    result.Add(new Position(testPosition));
+                }
+                else isEndLine = true;
+            }
+
+            testPosition = new Position(figurePosition);
+            isEndLine = false;
+            while (!isEndLine) //x down
+            {
+                testPosition.X = chessboard.DecrementX(testPosition.X);
+                if (testPosition.X != ' ' && chessboard.GetFigureByPosition(testPosition) == null)
+                {
+                    result.Add(new Position(testPosition));
+                }
+                else isEndLine = true;
+            }
+
+            return result;
         }
 
         public override IEnumerable<Position> GetAttackMoves(Position figurePosition, IChessboard chessboard)
         {
-            throw new System.NotImplementedException();
+            var result = new List<Position>();
+            var figure = chessboard.GetFigureByPosition(figurePosition);
+            var isEndLine = false;
+            var testPosition = new Position(figurePosition);
+
+            while (!isEndLine) //y up
+            {
+                testPosition.Y = chessboard.IncrementY(testPosition.Y);
+                if (testPosition.Y != -1)
+                {
+                    var testFigure = chessboard.GetFigureByPosition(testPosition);
+                    if (testFigure != null)
+                    {
+                        if (testFigure.Color != figure.Color)
+                        {
+                            result.Add(new Position(testPosition));
+                            isEndLine = true;
+                        }
+                        else
+                        {
+                            isEndLine = true;
+                        }
+                    }
+                }
+                else isEndLine = true;
+            }
+
+            testPosition = new Position(figurePosition);
+            isEndLine = false;
+            while (!isEndLine) //y down
+            {
+                testPosition.Y = chessboard.DecrementY(testPosition.Y);
+                if (testPosition.Y != -1)
+                {
+                    var testFigure = chessboard.GetFigureByPosition(testPosition);
+                    if (testFigure != null)
+                    {
+                        if (testFigure.Color != figure.Color)
+                        {
+                            result.Add(new Position(testPosition));
+                            isEndLine = true;
+                        }
+                        else
+                        {
+                            isEndLine = true;
+                        }
+                    }
+                }
+                else isEndLine = true;
+            }
+
+            testPosition = new Position(figurePosition);
+            isEndLine = false;
+            while (!isEndLine) //x up
+            {
+                testPosition.X = chessboard.IncrementX(testPosition.X);
+                if (testPosition.X != ' ')
+                {
+                    var testFigure = chessboard.GetFigureByPosition(testPosition);
+                    if (testFigure != null)
+                    {
+                        if (testFigure.Color != figure.Color)
+                        {
+                            result.Add(new Position(testPosition));
+                            isEndLine = true;
+                        }
+                        else
+                        {
+                            isEndLine = true;
+                        }
+                    }
+                }
+                else isEndLine = true;
+            }
+
+            testPosition = new Position(figurePosition);
+            isEndLine = false;
+            while (!isEndLine) //x down
+            {
+                testPosition.X = chessboard.DecrementX(testPosition.X);
+                if (testPosition.X != ' ')
+                {
+                    var testFigure = chessboard.GetFigureByPosition(testPosition);
+                    if (testFigure != null)
+                    {
+                        if (testFigure.Color != figure.Color)
+                        {
+                            result.Add(new Position(testPosition));
+                            isEndLine = true;
+                        }
+                        else
+                        {
+                            isEndLine = true;
+                        }
+                    }
+                }
+                else isEndLine = true;
+            }
+
+            return result;
         }
     }
 }
