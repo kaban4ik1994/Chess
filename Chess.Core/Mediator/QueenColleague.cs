8b0aec2bc0006810d1b483c243560f800bc5d5e7
Andrey <kaban4ik19901990@mail.ru>
2015-04-03 12:55:52 +0300

    Added move logic for queen.

--- Chess.Core/Mediator/QueenColleague.cs
+++ Chess.Core/Mediator/QueenColleague.cs
@@ -9,61 +9,33 @@ namespace Chess.Core.Mediator
         public override IEnumerable<Position> GetPossibleMoves(Position figurePosition, IChessboard chessboard)
         {
             var result = new List<Position>();
-            var isEndLine = false;
-            var testPosition = new Position(figurePosition);
 
-            while (!isEndLine) //y up
-            {
-                testPosition.Y = chessboard.IncrementY(testPosition.Y);
-                if (testPosition.Y != -1 && chessboard.GetFigureByPosition(testPosition) == null)
-                {
-                    result.Add(new Position(testPosition));
-                }
-                else isEndLine = true;
-            }
-
-            testPosition = new Position(figurePosition);
-            isEndLine = false;
-            while (!isEndLine) //y down
-            {
-                testPosition.Y = chessboard.DecrementY(testPosition.Y);
-                if (testPosition.Y != -1 && chessboard.GetFigureByPosition(testPosition) == null)
-                {
-                    result.Add(new Position(testPosition));
-                }
-                else isEndLine = true;
-            }
-
-            testPosition = new Position(figurePosition);
-            isEndLine = false;
-            while (!isEndLine) //x up
-            {
-                testPosition.X = chessboard.IncrementX(testPosition.X);
-                if (testPosition.X != ' ' && chessboard.GetFigureByPosition(testPosition) == null)
-                {
-                    result.Add(new Position(testPosition));
-                }
-                else isEndLine = true;
-            }
-
-            testPosition = new Position(figurePosition);
-            isEndLine = false;
-            while (!isEndLine) //x down
-            {
-                testPosition.X = chessboard.DecrementX(testPosition.X);
-                if (testPosition.X != ' ' && chessboard.GetFigureByPosition(testPosition) == null)
-                {
-                    result.Add(new Position(testPosition));
-                }
-                else isEndLine = true;
-            }
+            result.AddRange(GetPossibleMovesOnTheLineXAtDown(figurePosition, chessboard));
+            result.AddRange(GetPossibleMovesOnTheLineXAtDownAndYAtDown(figurePosition, chessboard));
+            result.AddRange(GetPossibleMovesOnTheLineXAtDownAndYAtUp(figurePosition, chessboard));
+            result.AddRange(GetPossibleMovesOnTheLineXAtUp(figurePosition, chessboard));
+            result.AddRange(GetPossibleMovesOnTheLineXAtUpAndYAtDown(figurePosition, chessboard));
+            result.AddRange(GetPossibleMovesOnTheLineXAtUpAndYAtUp(figurePosition, chessboard));
+            result.AddRange(GetPossibleMovesOnTheLineYAtDown(figurePosition, chessboard));
+            result.AddRange(GetPossibleMovesOnTheLineYAtUp(figurePosition, chessboard));
 
             return result;
         }
 
         public override IEnumerable<Position> GetAttackMoves(Position figurePosition, IChessboard chessboard)
         {
-            throw new System.NotImplementedException();
+            var result = new List<Position>();
+
+            result.AddRange(GetAttackMovesOnTheLineXAtDown(figurePosition, chessboard));
+            result.AddRange(GetAttackMovesOnTheLineXAtDownAndYAtDown(figurePosition, chessboard));
+            result.AddRange(GetAttackMovesOnTheLineXAtDownAndYAtUp(figurePosition, chessboard));
+            result.AddRange(GetAttackMovesOnTheLineXAtUp(figurePosition, chessboard));
+            result.AddRange(GetAttackMovesOnTheLineXAtUpAndYAtDown(figurePosition, chessboard));
+            result.AddRange(GetAttackMovesOnTheLineXAtUpAndYAtUp(figurePosition, chessboard));
+            result.AddRange(GetAttackMovesOnTheLineYAtDown(figurePosition, chessboard));
+            result.AddRange(GetAttackMovesOnTheLineYAtUp(figurePosition, chessboard));
+
+            return result;
         }
     }
 }
